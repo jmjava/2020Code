2e57d2a8d5c996001e0271a8c92c9a790d7db9f4
John Menke <jmjava@gmail.com>
2020-05-19 19:40:59 +0000

    format

--- Java/CountingSortInteger.java
+++ Java/CountingSortInteger.java
@@ -1,25 +1,26 @@
-public class CountingSortInteger{
+public class CountingSortInteger {
 
-
-public static void countingSort(int[] arr) {  int min = arr[0];  int max = arr[0];  for (int i = 1; i < arr.length; i++) {    if (arr[i] < min) {      min = arr[i];    } else if (arr[i] > max) {      max = arr[i];    }  }  int[] counts = new int[max - min + 1];  for (int i = 0; i < arr.length; i++) {    counts[arr[i] - min]++;  }  int sortedIndex = 0;  for (int i = 0; i < counts.length; i++) {    while (counts[i] > 0) {      arr[sortedIndex++] = i + min;      counts[i]--;    }  }}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    public static void countingSort(int[] arr) {
+        int min = arr[0];
+        int max = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] < min) {
+                min = arr[i];
+            } else if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        int[] counts = new int[max - min + 1];
+        for (int i = 0; i < arr.length; i++) {
+            counts[arr[i] - min]++;
+        }
+        int sortedIndex = 0;
+        for (int i = 0; i < counts.length; i++) {
+            while (counts[i] > 0) {
+                arr[sortedIndex++] = i + min;
+                counts[i]--;
+            }
+        }
+    }
 
 }
