41f3d486c4904bcc6f5b18f04a25063fd534d3e8
John Menke <jmjava@gmail.com>
2020-05-19 18:33:53 +0000

    Adding Counting Sort for Integer

---
+++ Java/CountingSortInteger.java
@@ -0,0 +1,25 @@
+public class CountingSortInteger{
+
+
+public static void countingSort(int[] arr) {  int min = arr[0];  int max = arr[0];  for (int i = 1; i < arr.length; i++) {    if (arr[i] < min) {      min = arr[i];    } else if (arr[i] > max) {      max = arr[i];    }  }  int[] counts = new int[max - min + 1];  for (int i = 0; i < arr.length; i++) {    counts[arr[i] - min]++;  }  int sortedIndex = 0;  for (int i = 0; i < counts.length; i++) {    while (counts[i] > 0) {      arr[sortedIndex++] = i + min;      counts[i]--;    }  }}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
